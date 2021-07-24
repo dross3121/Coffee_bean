7c6e4aa5792cfea200a503bac4f0add4389dd1a0
steveplayshorn <steveplayshorn@gmail.com>
2021-07-23 23:16:01 -0400

    Completed mobile nav.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,25 +1,48 @@
 import React from 'react'
 import './Nav.css'
 import { Link } from 'react-router-dom'
+import { Twirl as Hamburger } from 'hamburger-react'
+import { useState } from 'react'
 
 const Nav = () => {
+    const [isOpen, setOpen] = useState(false)
+
+    
+    
+   
+
     const handleBurger = () => {
-        const hamburgerMenu = document.querySelector('.hamburgerMenu')
-        hamburgerMenu.classList.toggle('active')
+        const nav = document.querySelector('nav')
+       nav.classList.toggle('active')
     }
+
+    const closeMobile = () => {
+        setOpen(false)
+        document.querySelector('nav').classList.remove('active')
+    }
+
     return (
         <nav>
             <Link to='/'>
-                <h1>COFFEE BEAN</h1>
+                <h1 onClick={closeMobile}>COFFEE BEAN</h1>
             </Link>
             <ul>
                 <Link to='/'><li>link1</li></Link>
                 <Link to='/'><li>link1</li></Link>
                 <Link to='/'><li>link1</li></Link>
                 <Link to='/cart'><li><i className="fas fa-shopping-cart"></i>(1)</li></Link>
-                <span className='hamburger' onClick={handleBurger}>=</span> 
             </ul>
-            <div className="hamburgerMenu">hiiii</div>
+            {isOpen ? <div className='hamburger-menu hamburger-container'>
+                <ul>
+                    <Link to='/'><li onClick={closeMobile}>link1</li></Link>
+                    <Link to='/'><li onClick={closeMobile}>link1</li></Link>
+                    <Link to='/'><li onClick={closeMobile}>link1</li></Link>
+                    <Link to='/cart'><li onClick={closeMobile}><i className="fas fa-shopping-cart"></i>(1)</li></Link>
+                </ul>
+            </div> : null}
+            <div onClick={handleBurger} className='hamburger-container'>
+                <Hamburger toggled={isOpen} toggle={setOpen} duration={0.2} />
+            </div>
         </nav>
     )
 }
